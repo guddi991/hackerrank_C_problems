e797d61f73ba7246074c183f1cd6b03ce3c006a5
guddi yadav <69110026+guddi991@users.noreply.github.com>
2021-09-12 12:21:22 +0530

    Create Grading Students.c

---
+++ Grading Students.c
@@ -0,0 +1,24 @@
+
+
+#include<stdio.h>
+int main(){
+    int n,g,ig;
+    scanf("%d",&n);
+    
+    for(int i=0;i<n;i++){
+        scanf("%d",&g);
+        if(g<38)
+          printf("%d\n",g);
+        else{
+            ig=(g/5+1)*5;
+            if(ig-g<3)
+            printf("%d\n",ig);
+            else {
+            printf("%d\n",g);
+            }
+            
+        }
+    }
+    return 0;
+    
+}
