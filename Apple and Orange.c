ba9555a83398fa7a6e68d636d7ea530d92b63cdd
guddi yadav <69110026+guddi991@users.noreply.github.com>
2021-09-17 17:58:04 +0530

    Create Apple and Orange.c

---
+++ Apple and Orange.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+
+int main(){
+    int s,t,a,b,m,n;
+    int apple=0,orange=0,x;
+    scanf("%d%d",&s,&t);
+    scanf("%d%d",&a,&b);
+    scanf("%d%d",&m,&n);
+    
+    for(int i=0;i<m;i++){
+        scanf("%d",&x);
+        if(x+a>=s&&x+a<=t)
+        apple++;
+    }
+    for(int i=0;i<n;i++){
+        scanf("%d",&x);
+        if(x+b>=s&&x+b<=t)
+        orange++;
+    }
+    printf("%d\n%d",apple,orange);
+    return 0;
+}
